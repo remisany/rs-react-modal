f18cda70bc1e5a6f4a91483ec632786cd8019dab
remisany <remi.d.sany@gmail.com>
2021-09-14 17:35:11 +0200

    Création example

---
+++ src/stories/modal.stories.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { useState, Fragment } from "react"
+import { storiesOf } from "@storybook/react"
+
+import Modal from "../components/Modal";
+
+const stories = storiesOf("Example", module);
+
+stories.add("App", () => {
+    const [active, setActive] = useState(false)
+
+    const close = () => {
+      setActive(!active)
+    }
+  
+    return (
+      <Fragment>
+        <div className = "container">
+          <h1>React Modal</h1>
+          <button onClick = {() => setActive(true)}>Save</button>
+          <Modal
+            open = {active}
+            content = "React modal is open !"
+            close = {close}
+          />
+        </div>
+      </Fragment>
+  
+    )
+});
